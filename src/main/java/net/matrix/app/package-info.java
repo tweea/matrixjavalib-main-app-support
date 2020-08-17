132d90160fd26464b5bbf80c7893696e5af939a7
Tweea <tweea@263.net>
2020-08-17 10:11:45 +0800

    更新版权信息

--- src/main/java/net/matrix/app/package-info.java
+++ src/main/java/net/matrix/app/package-info.java
@@ -1,6 +1,6 @@
 /*
- * Copyright(C) 2008 Matrix
- * All right reserved.
+ * 版权所有 2020 Matrix。
+ * 保留所有权利。
  */
 /**
  * 应用系统类，支持动态依赖管理概念。
--- src/main/java/net/matrix/app/repository/package-info.java
+++ src/main/java/net/matrix/app/repository/package-info.java
@@ -1,6 +1,6 @@
 /*
- * Copyright(C) 2008 Matrix
- * All right reserved.
+ * 版权所有 2020 Matrix。
+ * 保留所有权利。
  */
 /**
  * 应用仓储，用于访问任意仓储结构。
