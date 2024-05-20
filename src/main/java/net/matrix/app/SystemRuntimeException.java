eae3d0fe7fe3a464faa2dabd19beb0337cf84227
Tweea <tweea@qq.com>
2024-05-20 23:45:00 +0800

    修正 JavaDoc 错误

--- src/main/java/net/matrix/app/SystemRuntimeException.java
+++ src/main/java/net/matrix/app/SystemRuntimeException.java
@@ -48,10 +48,10 @@ public class SystemRuntimeException
     }
 
     /**
-     * 使用指定原因异常构造异常，详细信息指定为 <tt>(cause==null ? null : cause.toString())</tt> （特别地指定原因异常的类和详细信息）。
+     * 使用指定原因异常构造异常，详细信息指定为 <code>(cause==null ? null : cause.toString())</code> （特别地指定原因异常的类和详细信息）。
      * 
      * @param cause
-     *     原因异常（使用 {@link #getCause()} 方法获取）。可以使用 <tt>null</tt> 值，指原因异常不存在或未知。
+     *     原因异常（使用 {@link #getCause()} 方法获取）。可以使用 <code>null</code> 值，指原因异常不存在或未知。
      */
     public SystemRuntimeException(final Throwable cause) {
         super(cause);
@@ -69,7 +69,7 @@ public class SystemRuntimeException
      * 注意与 <code>cause</code> 关联的详细信息<i>不会</i>自动出现在本异常的详细信息中。
      * 
      * @param cause
-     *     原因异常（使用 {@link #getCause()} 方法获取）。可以使用 <tt>null</tt> 值，指原因异常不存在或未知。
+     *     原因异常（使用 {@link #getCause()} 方法获取）。可以使用 <code>null</code> 值，指原因异常不存在或未知。
      * @param messageCode
      *     消息编码。
      */
@@ -89,7 +89,7 @@ public class SystemRuntimeException
      * 注意与 <code>cause</code> 关联的详细信息<i>不会</i>自动出现在本异常的详细信息中。
      * 
      * @param cause
-     *     原因异常（使用 {@link #getCause()} 方法获取）。可以使用 <tt>null</tt> 值，指原因异常不存在或未知。
+     *     原因异常（使用 {@link #getCause()} 方法获取）。可以使用 <code>null</code> 值，指原因异常不存在或未知。
      * @param message
      *     消息。
      */
