611dde813351a1ac420851a50d10796e393d7d28
Tweea <tweea@qq.com>
2024-05-25 15:54:32 +0800

    GlobalSystemContext

--- src/main/java/net/matrix/app/GlobalSystemContext.java
+++ src/main/java/net/matrix/app/GlobalSystemContext.java
@@ -1,5 +1,5 @@
 /*
- * 版权所有 2020 Matrix。
+ * 版权所有 2024 Matrix。
  * 保留所有权利。
  */
 package net.matrix.app;
@@ -23,47 +23,44 @@ public final class GlobalSystemContext {
     }
 
     /**
-     * 获取系统环境的全局实例，如果不存在则建立默认的系统环境实例。
+     * 获取系统环境的全局实例，使用缺省的唯一标识，如果不存在则建立默认的系统环境实例。
      * 
-     * @return 系统环境的全局实例
+     * @return 系统环境的全局实例。
      */
     public static SystemContext get() {
         return get("");
     }
 
     /**
-     * 获取系统环境的全局实例，如果不存在则建立默认的系统环境实例。
+     * 获取系统环境的全局实例，使用指定的唯一标识，如果不存在则建立默认的系统环境实例。
      *
      * @param id
-     *     系统环境 ID
-     * @return 系统环境的全局实例
+     *     唯一标识。
+     * @return 系统环境的全局实例。
      */
-    public static SystemContext get(final String id) {
-        if (!GLOBAL_CONTEXTS.containsKey(id)) {
-            GLOBAL_CONTEXTS.putIfAbsent(id, new DefaultSystemContext());
-        }
-        return GLOBAL_CONTEXTS.get(id);
+    public static SystemContext get(String id) {
+        return GLOBAL_CONTEXTS.computeIfAbsent(id, key -> new DefaultSystemContext());
     }
 
     /**
-     * 设置系统环境的全局实例。
+     * 设置系统环境的全局实例，使用缺省的唯一标识。
      * 
      * @param context
-     *     系统环境
+     *     系统环境。
      */
-    public static void set(final SystemContext context) {
+    public static void set(SystemContext context) {
         set("", context);
     }
 
     /**
-     * 设置系统环境的全局实例。
+     * 设置系统环境的全局实例，使用指定的唯一标识。
      * 
      * @param id
-     *     系统环境 ID
+     *     唯一标识。
      * @param context
-     *     系统环境
+     *     系统环境。
      */
-    public static void set(final String id, final SystemContext context) {
+    public static void set(String id, SystemContext context) {
         if (context == null) {
             GLOBAL_CONTEXTS.remove(id);
         } else {
